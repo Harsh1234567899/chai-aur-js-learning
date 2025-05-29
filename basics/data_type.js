af6352c664891e9d207665e3fbd3e05da60bd786
Pankhaniya Harsh <118796163+Harsh1234567899@users.noreply.github.com>
2025-05-29 07:42:13 +0000

    day 1

---
+++ basics/data_type.js
@@ -0,0 +1,14 @@
+"use strict" // treat all JS code as newer version
+
+//alert () // can't use in node // use but in different way
+
+console.log(3+3); console.log(3-3); // ; is use when multiple statement is write in one line 
+
+/*
+numbers -> 2 to 53
+bigint
+string
+boolean = true / false
+null // stand alone value
+undefined // vale that not defined 
+*/
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 123456
+let accountEmail = "harsh@gmail.com"
+var accPass = "123456"
+accCity = "rajkot"
+let accState;
+// accountId = 654321  // not allowed
+accountEmail = "abc@gmail.com"
+accPass = "654321"
+accCity = "porbander"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accPass,accCity,accState]) // print all values in table formate 
