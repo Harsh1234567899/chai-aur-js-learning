03e17bb394f7926a1c49714c1e2a7c7db09474ed
Pankhaniya Harsh <118796163+Harsh1234567899@users.noreply.github.com>
2025-06-04 13:32:14 +0000

    functions and variable scope

--- basics/11_funtions.js
+++ basics/11_funtions.js
@@ -4,3 +4,31 @@
 
 // const mera = myFun(5,6) // direct print console of funtion dont wait for variable print
 
+// function cart(...num1){ // using spread operator we can accept argument infinite
+//     return num1
+// }
+// console.log(cart(100,200,300)); // but value come in arrays formate
+
+//********************************************** 
+const user = {
+    username: "harsh",
+    price: 200
+}
+
+function myCart(anyObject){
+    console.log(`userrname is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// myCart(user)
+// myCart({  // direct add aa object
+//     username: "pankhaniya",
+//     price: 200
+// })
+
+const myArr = [100 , 200 , 400]
+
+function myArrayFunction(num1){
+    return num1[1]
+}
+console.log(myArrayFunction(myArr));
+console.log(myArrayFunction([500,600,700]));//passing array in arrgument
