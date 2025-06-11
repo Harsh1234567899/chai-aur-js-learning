a52c142a4194d1ae805e49afe8ab1fe943dde12d
Pankhaniya Harsh <118796163+Harsh1234567899@users.noreply.github.com>
2025-06-11 13:20:33 +0000

    for and while loops

---
+++ loops_iterations/for_loop.js
@@ -0,0 +1,33 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(i);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("this is  5");        
+//     }
+//     console.log(i);
+// }
+
+for (let i = 0; i < 10; i++) {
+    // console.log(`outer is ${i}`);
+    
+    for (let j = 0; j < 10; j++) {
+        // console.log(`inner ${i} and ${j}`);
+        // console.log(i + "*" + j +  '=' + i*j);
+        
+    }
+    
+}
+
+let array =  ["harsh","pankhaniya","hello"]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    // console.log(element);    
+}
+
+//
---
+++ loops_iterations/while.js
@@ -0,0 +1,20 @@
+// let i = 20
+// while (i <= 20) {
+//     console.log(i);
+//     break
+// }
+
+// let myArr = ["harsh","pankhaniya"]
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr +1
+    
+// }
+
+let score = 1
+do {
+    console.log(score);
+    score++
+    
+} while (score <= 10);
